7e57f1939b7727bbd1b6fbe964235208846d4382
ewdwins <saleh.tarkashvand@gmail.com>
2025-08-05 17:03:15 +0330

    add image-sildeShow and create main-header-background

---
+++ components/main-Header/main-header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+
+function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">foodies community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default MainHeader;
